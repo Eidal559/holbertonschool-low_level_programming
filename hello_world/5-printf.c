21a51e714247f8b3a7fd3e047b02ff1861267cc6
Eidal559 <eidaljuarbe@gmail.com>
2023-10-12 06:40:02 -0700

    helloworld5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints a string with proper grammar
+ * but the outcome is a piece of art
+ *
+ * return: Always 0 (Success)
+ */
 int main()
 {
-  printf("This is a piece of art\n");
-  return 0;
+printf("with a proper grammar, but the outcome is a piece of art,\n");
+return 0;
 }
