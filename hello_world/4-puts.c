8ca101fc86020fc6a069f79eb9130ee2607ebb29
Eidal559 <eidaljuarbe@gmail.com>
2023-10-12 06:14:22 -0700

    helloworld4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point 
  *
- *Description: prints a string followed by a newline
+ * Description: prints a string followed by a newline
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
- puts("\"programming is like building a multilingual puzzle");
- return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
