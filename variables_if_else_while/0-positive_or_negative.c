4433adf5979ef3ed3f879c19b0edbe08a2270123
Eidal559 <eidaljuarbe@gmail.com>
2023-10-16 10:09:54 -0700

    view0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,24 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+/**
+ *main - Entry point
+ *
+ *Prints whether the number n is positive, negative, or zero
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
  int n;
   srand(time(0));
- 
-  n = rand();
- 
- printf("The number %d is ", n);
- 
- if (n > 0){ 
-   printf("is positive\n,", n);
+  n = rand() - RAND_MAX / 2;
+  
+ /* your code goes there */
+ if (n > 0)
+ { 
+   printf("%d is positive\n,", n);
  }
- else if (n < 0){
-   printf("is negative\n", n);
+ else if (n < 0)
+ {
+   printf("%d is negative\n", n);
  }
- else{
-   printf("is zero\n", n);
+ else
+ {
+   printf("%d is zero\n", n);
  }
  return (0);
 }
