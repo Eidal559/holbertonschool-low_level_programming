2984b570d933d0a92dc9c673b55846647fb9ff53
Eidal559 <eidaljuarbe@gmail.com>
2023-10-16 16:03:12 -0700

    view1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,13 +9,12 @@
 *
 * Return: Always 0 (Success)
 */
-/* The main function is the entry point of the program */
 int main(void) 
 {
 int n, last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10; /* Getting the last digit of n */
+last_digit = n % 10;
 printf("Last digit of %d is %d and is ", n, last_digit);
 
 if (last_digit > 5)
