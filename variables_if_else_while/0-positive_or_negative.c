1d138c6b61011a797140b9f2e79c1b40cf074adc
Eidal559 <eidaljuarbe@gmail.com>
2023-10-16 08:58:33 -0700

    view0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 int main(void)
 {
  int n;
- /* Initialize the random number generator with the current time */ srand(time(NULL));
- /* Generate a ramdom number between INT_MIN and INT_MAX */
+  srand(time(0));
+ 
   n = rand();
- /* Print the random number */
+ 
  printf("The number %d is ", n);
- /* Check if the number is positive, negative, or zero */
- if (n > 0) 
+ 
+ if (n > 0){ 
    printf("is positive\n,", n);
- else if (n < 0)
+ }
+ else if (n < 0){
    printf("is negative\n", n);
+ }
  else{
    printf("is zero\n", n);
+ }
  return (0);
 }
