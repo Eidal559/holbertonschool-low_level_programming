1858cf7d32329200b494eaaa2be2893f0d84ddd6
Eidal559 <eidaljuarbe@gmail.com>
2023-10-17 07:16:38 -0700

    view5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: This program prints all single-digit numbers of base 10.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
