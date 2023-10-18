e7694138c0b222a37194df965c6603d0f456387c
Eidal559 <eidaljuarbe@gmail.com>
2023-10-18 12:07:56 -0700

    loop3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
 * _islower - check if a character is lowercase.
+* @c: The character to check
 *
 * Description: This function checks if the character 'c' is a lowercase
 * Return: 1 if 'c' is lowercase, 0 otherwise.
