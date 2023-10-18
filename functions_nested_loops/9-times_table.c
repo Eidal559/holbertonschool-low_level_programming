7dfbf3c8c988eced5b59a1bcaf662133dcb931ee
Eidal559 <eidaljuarbe@gmail.com>
2023-10-18 15:46:08 -0700

    loop9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 int row, column, result;
 for (row = 0; row <= 9; row++)
 {
-for ( column = 0; column <= 9; column++)
+for (column = 0; column <= 9; column++)
 {
 result = row * column;
 if (column == 0)
