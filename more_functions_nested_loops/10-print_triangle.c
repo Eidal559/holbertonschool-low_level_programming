764169bd99fd33f7b5951cc7496cad3073bc33f5
Eidal559 <eidaljuarbe@gmail.com>
2023-10-21 17:03:05 -0700

    moreloops10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle made of '#'.
+* @size: The size of the triangle.
+*
+* Return: 0
+*/
+
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int row, col;
+for (row = 1; row <= size; row++) {
+for (col = 1; col <= size; col++) {
+if (col <= size - row) {
+_putchar(' ');
+}
+else {
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}
