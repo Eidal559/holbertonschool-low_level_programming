d464370f220e3fd6623cb096b1b0ec08fa8d7fa8
Eidal559 <eidaljuarbe@gmail.com>
2023-10-23 07:09:09 -0700

    arrays0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -5,9 +5,9 @@
 * reset_to_98 - Update the value pointed to by an integer pointer to 98.
 * @ptr: A pointer to an integer.
 *
-* This function takes a pointer to an integer and updates the value it points to 98.
+* This function takes a pointer to an integer and updates the value to 98.
 *
-* Return: 0
+* n - is NULL
 */
 
 void reset_to_98(int *n)
