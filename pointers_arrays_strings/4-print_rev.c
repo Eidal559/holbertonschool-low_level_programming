8b3d232e0faf1e0029f1fe71c08b71ec08bc8051
Eidal559 <eidaljuarbe@gmail.com>
2023-10-23 11:50:16 -0700

    arrays4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* print_rev - prints a string in reverse order followed by a newline
+* @s: The string to be printed in reverse.
+*/
+
+void print_rev(char *s)
+{
+int length = strlen(s);
+
+for (int i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
