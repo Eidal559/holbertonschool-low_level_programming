8416bb69b9fb0527726c2ecfabf48f20c7896baa
Eidal559 <eidaljuarbe@gmail.com>
2023-10-23 12:51:41 -0700

    arrays7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,8 @@ void puts_half(char *str)
 {
 int length = strlen(str);
 int start;
-
+int i;
+ 
 if (length % 2 == 0)
 {
 start = length / 2;
@@ -20,7 +21,7 @@ else
 {
 start = (length - 1) / 2;
 }
-for (int i = start; i < length; i++)
+for (i = start; i < length; i++)
 {
 putchar(str[i]);
 }
