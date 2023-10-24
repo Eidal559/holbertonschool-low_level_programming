4d03ec61ddc3781bb926069aabf6818c6e9c6403
Eidal559 <eidaljuarbe@gmail.com>
2023-10-24 11:24:27 -0700

    more arrays6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* cap_string - Capitalizes the first letter of each word in a string.
+*
+* @str: The string to be capitalized.
+*
+* Return: A pointer to the modified string.
+*/
+
+char *cap_string(char *str)
+{
+int capitalize_next = 1;
+int i = 0;
+int is_separator(char c);
+
+while (str[i] != '\0')
+{
+if (is_separator(str[i]))
+{
+capitalize_next = 1;
+}
+else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+capitalize_next = 0;
+}
+else
+{
+capitalize_next = 0;
+}
+i++;
+}
+return (str);
+}
