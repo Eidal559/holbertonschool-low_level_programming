719eb8a03623f07c684724fb8ce8d87b1ec4845f
Eidal559 <eidaljuarbe@gmail.com>
2023-10-24 08:56:18 -0700

    more arrays3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,12 +16,12 @@ int i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
-return s1[i] - s2[i];
+return (s1[i] - s2[i]);
 i++;
 }
 if (s1[i] != '\0')
-return s1[i];
+return (s1[i]);
 if (s2[i] != '\0')
-return -s2[i];
+return (-s2[i]);
 return (0);
 }
