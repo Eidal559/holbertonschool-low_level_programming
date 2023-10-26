db2e381682e3ae3d396dbf74cdc2dc3a6ce866a0
Eidal559 <eidaljuarbe@gmail.com>
2023-10-26 07:25:41 -0700

    pointers7

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,5 +30,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *neddle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void print_chessboard(void);
 
 #endif
