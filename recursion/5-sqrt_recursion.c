f77ef1167ee4431b4fd027091af381743fd5691e
Eidal559 <eidaljuarbe@gmail.com>
2023-10-30 12:23:11 -0700

    recursion5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,15 +9,19 @@
 
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-return (find_sqrt(n, 1));
-}
 int find_sqrt(int n, int guess)
 {
 if (guess * guess == n)
-return guess;
+return (guess);
 if (guess * guess > n)
 return (-1);
+
 return (find_sqrt(n, guess + 1));
+}
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+
+return (find_sqrt(n, 1));
 }
