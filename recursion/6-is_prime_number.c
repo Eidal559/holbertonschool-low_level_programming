ee3a503f5925be54a3fab15cb57b66502531da89
Eidal559 <eidaljuarbe@gmail.com>
2023-10-31 07:32:37 -0700

    recursion6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,24 +2,33 @@
 #include <stdio.h>
 
 /**
-* is_prime_number - checks if a number is a prime number.
-* @n: The number to check.
-* Return: 1 if n is a prime number, 0 otherwise.
+* is_divisible - Helper function to check if n is divisible by divisor.
+* @n: The number to check for divisibility.
+* is_prime_number - Checks if an integer is a prime number.
+* Return: 1 if n is divisible by divisor, 0 otherwise.
 */
+
+int is_divisible(int n, int divisor)
+{
+if (divisor == 1)
+return (0);
+
+if (n % divisor == 0)
+return (1);
+
+if (divisor * divisor > n)
+return (0);
+
+return (is_divisible(n, divisor + 1));
+}
 
 int is_prime_number(int n)
 {
-int i;
 if (n <= 1)
 return (0);
+
 if (n <= 3)
 return (1);
-if (n % 2 == 0 || n % 3 == 0)
-return (0);
-for (i = 5; i * i <= n; i += 6)
-{
-if (n % i == 0 || n % (i + 2) == 0)
-return (0);
-}
-return (1);
+
+return (!is_divisible(n, 2));
 }
