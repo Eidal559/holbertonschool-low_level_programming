cac136d127bfd2d1453a47e02a78fcef3f1c932c
Eidal559 <eidaljuarbe@gmail.com>
2023-11-01 07:57:19 -0700

    arguments1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -12,9 +12,9 @@
 int main(int argc, char *argv[])
 {
 int i;
-printf("Number of arguments: %d\n", argc);
+printf("Number of arguments: %d\n", argc - 1);
 
-for (i = 0; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
 printf("Argument %d: %s\n", i, argv[i]);
 }
