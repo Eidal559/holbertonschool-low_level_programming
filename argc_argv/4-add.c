5d2ddb0bf074508de52a45f1fb2a00757cb1c879
Eidal559 <eidaljuarbe@gmail.com>
2023-11-01 09:09:02 -0700

    arguments4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+* main - Entry point of the program
+* @argc: The number of command-line arguments
+* @argv: An array of strings containing the command-line argumments
+*
+* Return: 0 if successful, 1 if there is an error
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+int sum = 0;
+bool error = false;
+
+for (int i = 1; i < argc; i++)
+{
+for (int j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+error = true;
+break;
+}
+}
+if (error)
+{
+printf("Error\n");
+return (1);
+}
+int num = atoi(argv[i]);
+if (num < 0)
+{
+printf("Error\n");
+return (1);
+}
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
