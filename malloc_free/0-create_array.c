11d7940aa239f82e93bb115e29627d067a493618
Eidal559 <eidaljuarbe@gmail.com>
2023-11-06 07:11:59 -0800

    malloc0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,14 +14,14 @@
 char *create_array(unsigned int size, char c)
 {
 char *array;
-
+int i;
 if (size == 0)
 return (NULL);
 array = (char *)malloc(size * sizeof(char));
 if (array == NULL)
 return (NULL);
 
-for (unsigned int i = 0; i < size; i++)
+for (unsigned i = 0; i < size; i++)
 {
 write(STDOUT_FILENO, &array[i], 1);
 write(STDOUT_FILENO, " ", 1);
