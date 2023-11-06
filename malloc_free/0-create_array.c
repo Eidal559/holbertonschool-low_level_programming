d4939a2db8bb9ee047390f8c72b5da869249f32a
Eidal559 <eidaljuarbe@gmail.com>
2023-11-06 07:48:25 -0800

    malloc0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,48 +8,24 @@
 * @c: The character to initialize the array with.
 *
 * Return: if memory allocation fails or if size is 0, return NULL.
+*         otherwise, return a pointer to the created array.
 */
 
 char *create_array(unsigned int size, char c)
 {
-char *array;
 unsigned int i;
+char *array;
+
 if (size == 0)
 return (NULL);
+
 array = (char *)malloc(size * sizeof(char));
 if (array == NULL)
 return (NULL);
+
 for (i = 0; i < size; i++)
 {
 array[i] = c;
 }
-
 return (array);
-}
-int main(void)
-{
-unsigned int size = 10;
-char character = 'A';
-unsigned int i;
-char *array;
-
-array = create_array(size, character);
-
-if (array == NULL)
-{
-write(STDOUT_FILENO, "Memory allocation failed.\n", 26);
-}
-else
-{
-write(STDOUT_FILENO, "Array created: ", 15);
-
-for (i = 0; i < size; i++)
-{
-write(STDOUT_FILENO, &array[i], 1);
-write(STDOUT_FILENO, " ", 1);
-}
-write(STDOUT_FILENO, "\n", 1);
-free(array);
-}
-return (0);
 }
