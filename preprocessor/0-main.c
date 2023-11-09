f7e93192c3070f29da6c9e92d17afb6f084a5a7e
Eidal559 <eidaljuarbe@gmail.com>
2023-11-09 09:33:15 -0800

    preprocessor0

---
+++ preprocessor/0-main.c
@@ -0,0 +1,18 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints the sum of 98 and SIZE
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int s;
+
+s = 98 + SIZE;
+printf("%d\n", s);
+return (0);
+}
