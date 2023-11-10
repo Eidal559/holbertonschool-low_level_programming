2784384af44ce907f751f6e6a0d2718fa6419bab
Eidal559 <eidaljuarbe@gmail.com>
2023-11-10 04:44:34 -0800

    preprocessor4

---
+++ preprocessor/4-main.c
@@ -0,0 +1,20 @@
+#include "4-sum.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Uses the SUM macro to compute and print
+* the sum of 98 and 1024.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int s;
+
+s = SUM(98, 1024);
+printf("%d\n", s);
+return (0);
+}
