de7bc5adad2d5c967a1d58f293c6b8ed382d0e24
Eidal559 <eidaljuarbe@gmail.com>
2023-11-13 06:13:57 -0800

    structures0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Define a structure to represent information about dog.
+*/
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
