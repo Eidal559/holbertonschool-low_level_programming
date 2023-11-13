ba7803fb373660d4af24071278394c9630ccf477
Eidal559 <eidaljuarbe@gmail.com>
2023-11-13 06:17:04 -0800

    structures0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,6 +3,9 @@
 
 /**
 * struct dog - Define a structure to represent information about dog.
+* @name: pointer to a character array holding the dog name.
+* @age: The age of the dog as a float.
+* @owner: Pointer to a character array holding the dog owner name.
 */
 
 struct dog
