657741f9f0522161e3f853a789633b9d9929c62a
Eidal559 <eidaljuarbe@gmail.com>
2023-11-13 09:19:45 -0800

    structures4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+* new_dog - Create a new dog and initialize its members.
+* @name: Pointer to the character array for the dog name.
+* @age: The age of the dog as a float.
+* @owner: Pointer to the character array for the dog owner name.
+*
+* Return: Pointer to the new dog struct if successful, or null.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog_ptr;
+char *name_copy, *owner_copy;
+
+new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
+return (NULL);
+
+name_copy = strdup(name);
+if (name_copy == NULL)
+{
+free(new_dog_ptr);
+return (NULL);
+}
+
+owner_copy = strdup(owner);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(new_dog_ptr);
+return (NULL);
+}
+
+new_dog_ptr->name = name_copy;
+new_dog_ptr->age = age;
+new_dog_ptr->owner = owner_copy;
+
+return (new_dog_ptr);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -32,4 +32,14 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 */
 void print_dog(struct dog *d);
 
+/**
+* new_dog - Create a new dog and initialize its members.
+* @name: Pointer to the character array for the dog name.
+* @age: The age of the dog as a float.
+* @owner: Pointer to the character array for the dog owner name.
+*
+* Return: Pointer to the new dog struct if successful, or NULL.
+*/
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
