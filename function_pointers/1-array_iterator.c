891590de441aa3f1170fbabddbf942b22374fa41
Eidal559 <eidaljuarbe@gmail.com>
2023-11-14 06:25:45 -0800

    function_pointers1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,9 +10,11 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+int size_t;
+int i;
 if (array != NULL && action != NULL)
 {
-for (size_t i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
