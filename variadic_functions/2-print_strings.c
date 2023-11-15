459d24dd74b616c226fe1c58b6e693896de0e143
Eidal559 <eidaljuarbe@gmail.com>
2023-11-15 07:29:37 -0800

    var_func2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -6,8 +6,10 @@
 * @separator: The string to be printed between strings (or NULL)
 * @n: The number of strings passed to the function.
 * @...: The strings to be printed.
-* Description: This function takes a variable number of strings
+* print_string: This function takes a variable number of strings
 * and prints them with the specified separator between them.
+* If a separator is provided, it is printed between strings. If
+* a string is NULL, it prints "(nil)". new line is printed at the end
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
