61d51b2b120b1dde33c512c7fc5ee81d00ce3b94
Eidal559 <eidaljuarbe@gmail.com>
2023-11-15 06:11:30 -0800

    var_func0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,11 +15,13 @@ return (0);
 }
 
 int sum = 0;
+unsigned int i;
+
 va_list args;
 
 va_start(args, n);
 
-for (unsigned int i = 0; i < n; ++i)
+for (i = 0; i < n; ++i)
 {
 sum += va_arg(args, int);
 }
