63a4e44eefd36217ba7055611d55453fa7cfbbdf
Eidal559 <eidaljuarbe@gmail.com>
2023-11-28 10:28:55 -0800

    linklist3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,15 +4,15 @@
 
 /**
 * add_node_end - Adds a new node at the end of a list_t list.
-* @head: A pointer to the head of the linked list.
-* @str: The string to be duplicated and added to the new node.
+* @head: A pointer to the head of the list.
+* @str: string to be added to the list.
 *
 * Return: The address od the new element, or NULL if it failed.
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node;
-list_t *current;
+list_t *new_node, *last_node;
+unsigned int len = 0;
 
 if (str == NULL)
 return (NULL);
@@ -28,21 +28,23 @@ free(new_node);
 return (NULL);
 }
 
-new_node->len = strlen(str);
+while (str[len])
+len++;
+
+new_node->len = len;
 new_node->next =  NULL;
 
 if (*head == NULL)
 {
 *head = new_node;
+return (new_node);
 }
-else
-{
-current = *head;
-while (current->next != NULL)
-{
-current = current->next;
-}
-current->next = new_node;
-}
+
+last_node = *head;
+while (last_node->next != NULL)
+last_node = last_node->next;
+
+last_node->next = new_node; 
+
 return (new_node);
 }
