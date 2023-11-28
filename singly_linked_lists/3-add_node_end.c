5b7a721fd2a89e941ed2a76a548e1b4cdce49037
Eidal559 <eidaljuarbe@gmail.com>
2023-11-28 08:36:38 -0800

    linklist3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,15 +11,25 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
+list_t *new_node;
+list_t *current;
+
 if (str == NULL)
 return (NULL);
 
-list_t *new_node = malloc(sizeof(list_t));
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL);
 return (NULL);
 
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
 new_node->len = strlen(str);
-new_node->next = NULL;
+new_node->next =  NULL;
 
 if (*head == NULL)
 {
@@ -27,7 +37,7 @@ if (*head == NULL)
 }
 else
 {
-list_t *current = *head;
+current = *head;
 while (current->next != NULL)
 {
 current = current->next;
