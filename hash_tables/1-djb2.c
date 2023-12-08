d47c7ffbfb7aa7da98114b2c51874ba622314729
Eidal559 <eidaljuarbe@gmail.com>
2023-12-08 06:49:41 -0800

    hash_table-1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+* hash_djb2 - DJB2 hash function implementation
+* @str: The input string to hash
+*
+* Return: The computed hash value
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381;
+int c;
+
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c;
+}
+
+return (hash);
+}
