15d3ff6e44c6541332919a6cd89da8fc7cce0613
Eidal559 <eidaljuarbe@gmail.com>
2024-01-16 10:25:51 -0400

    update

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,24 +10,24 @@
  * Return: The index where the value is located, or -1 if not found or array is NULL.
  */
 int linear_search(int *array, size_t size, int value) {
-    size_t i;  /* Move the variable declaration outside the loop */
+size_t i;  /* Move the variable declaration outside the loop */
 
-    /* Check for NULL array */
-    if (array == NULL) {
-        return (-1);
-    }
+/* Check for NULL array */
+if (array == NULL) {
+return (-1);
+}
 
-    /* Iterate through the array to find the value */
-    for (i = 0; i < size; ++i) {
-        /* Print the value being compared */
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+/* Iterate through the array to find the value */
+for (i = 0; i < size; ++i) {
+/* Print the value being compared */
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        /* If the value is found, return the index */
-        if (array[i] == value) {
-            return (i);
-        }
-    }
+/* If the value is found, return the index */
+if (array[i] == value) {
+return (i);
+}
+}
 
-    /* If the value is not found, return -1 */
-    return (-1);
+/* If the value is not found, return -1 */
+return (-1);
 }
