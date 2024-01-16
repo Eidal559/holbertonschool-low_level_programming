cfa179c6eb3dc370b361b9ea776bdbe8a4abd674
Ladie juarbe <139393459+Eidal559@users.noreply.github.com>
2024-01-16 11:19:59 -0400

    binary update 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,54 +1,39 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array using binary search.
+ * binary_search - This function searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for
  *
- * @array: Pointer to the first element of the array to search in (sorted in ascending order).
- * @size: Number of elements in the array.
- * @value: The value to search for.
- *
- * Return: The index where the value is located, or -1 if not found or array is NULL.
+ * Return: Index where value is located
  */
-int binary_search(int *array, size_t size, int value) 
+
+int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, mid;
-    int i;
+	size_t i, left, right;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
 
-    left = 0;
-    right = size - 1;
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-    while (left <= right)
-    {
-        mid = left + (right - left) / 2;
+		i = left + (right - left) / 2;
 
-        printf("Searching in array: ");
-        for (i = left; i <= right; ++i) 
-        {
-            printf("%d", array[i]);
-            if (i < right) {
-                printf(", ");
-            }
-        }
-        printf("\n");
-
-        if (array[mid] == value) 
-        {
-            printf("Found at index: %lu\n", mid);
-            return mid;
-        } else if (array[mid] > value) 
-        {
-            right = mid - 1;
-        } else 
-        {
-            left = mid + 1;
-        }
-    }
-
-    printf("Not found\n");
-    return (-1);
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
 }
